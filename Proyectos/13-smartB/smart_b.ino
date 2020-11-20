e5f676ef542f166809ac79e1686f698266fdd910
MAIALERT <mairatovar56@gmail.com>
2020-11-19 22:27:33 -0500

    add Codigo

---
+++ Proyectos/13-smartB/smart_b.ino
@@ -0,0 +1,17 @@
+
+const int relay = 14;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(relay, LOW);
+  Serial.println("Current Flowing");
+  delay(5000); 
+  
+  digitalWrite(relay, HIGH);
+  Serial.println("Current not Flowing");
+  delay(5000);
+}
